a8d66055d9b4b206dff899ae6b08f5bace0decfd
Inder <iphone201988@gmail.com>
2026-02-17 15:38:21 +0530

    changes in uploads

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
 const options = {
   key: fs.readFileSync(path.resolve(__dirname, "../ssl/private.key"), 'utf8'),
   cert: fs.readFileSync(path.resolve(__dirname, "../ssl/certificate.crt"), 'utf8'),
