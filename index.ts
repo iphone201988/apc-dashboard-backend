6f6cd26dd84508757125c34d46b0ec2b0a267731
Inder <iphone201988@gmail.com>
2026-02-20 18:39:00 +0530

    changes in icons

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 const options = {
   key: fs.readFileSync(path.resolve(__dirname, "../ssl/private.key"), 'utf8'),
   cert: fs.readFileSync(path.resolve(__dirname, "../ssl/certificate.crt"), 'utf8'),
@@ -29,12 +29,12 @@ connectToDB()
   .then(() => {
     console.log("Connected to DB successfully", process.env.MONGO_URI);
 
-    // http.createServer(app).listen(port, () => {
-    //   console.log(`HTTP Server on ${port}`);
-    // });
-    https.createServer(options, app).listen(8000, () => {
-      console.log("HTTPS Server on 8000");
+    http.createServer(app).listen(port, () => {
+      console.log(`HTTP Server on ${port}`);
     });
+    // https.createServer(options, app).listen(8000, () => {
+    //   console.log("HTTPS Server on 8000");
+    // });
   })
   .catch((error) => {
     console.log("Error connecting to DB", error);
